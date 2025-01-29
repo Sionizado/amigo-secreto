02da087b10ca271dcee0968b1e4b26c702d8e74d
sionizado <klugjavo@gmail.com>
2025-01-29 09:50:11 -0300

    Agregar los amigos a una lista en el html

--- app.js
+++ app.js
@@ -3,23 +3,35 @@
 //Variables
 let amigos = [];
 let amigo = document.getElementById('amigo');
-
+let lista = document.getElementById('listaAmigos');
 
 //agrego un amigo si el valor no es vacio
 
 function agregarAmigo() {
-  
+  //TODO verificar que no sean números ni símbolos raros, ni duplicados
   if(amigo.value != '') {
     amigos.push(amigo.value);
     
     console.log(amigos);
     limpiarCaja();
+    agregarLista();
   } else {
     alert('Por favor, inserte un nombre');
   }
   
 }
 
+// limpia el input
 function limpiarCaja(){
   amigo.value = '';
+}
+
+//agrega un nuevo amigo a la lista
+let agregarLista = () => {
+  lista.innerHTML = '';
+  for (let i = 0; i < amigos.length; i++) {
+    let nuevoAmigo = document.createElement('li');
+    nuevoAmigo.textContent = amigos[i];
+    lista.appendChild(item);
+  }
 }
